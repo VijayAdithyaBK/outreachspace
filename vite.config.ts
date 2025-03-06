71600882df4f02f2285a9c679f7c91da5f3d9cb3
Vijay Adithya B K <vijayadithyabk@gmail.com>
2025-03-07 00:49:57 +0530

    github pages initialization

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Detect if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  // base: '/outreachspace/',
+  base: isProduction ? '/outreachspace/' : '/', // Only use base for GitHub Pages
+
 });
